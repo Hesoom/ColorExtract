ece9125b47e32a8251726037146f614a5cfaa0fb
Hesam Ro <hesamroustaei80@gmail.com>
2025-07-31 23:14:08 +0330

    Add uploading image

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, request, render_template, send_from_directory
+import os
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 
-@app.route("/")
+UPLOAD_FOLDER = 'uploads'
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
+
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+
+# Helper function to check valid file type
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route("/", methods=['GET', 'POST'])
 def home():
     color_data = {
         "#481830": 0.301746,
@@ -16,8 +28,21 @@ def home():
         "#781818": 0.017857,
         "#f0d860": 0.007857
     }
+    filename = None
+    if request.method == 'POST':
+        file = request.files.get('image')
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            file.save(file_path)
+    print(filename)
+    return render_template("index.html",colors=color_data, filename=filename)
 
-    return render_template("index.html",colors=color_data)
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+    
