8dd52c4b03760f2aaf057d9208a5de429cd738ab
Hesam Ro <hesamroustaei80@gmail.com>
2025-08-01 09:36:39 +0330

    process the image and show top 10 commmon colors

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 from flask import Flask, request, render_template, send_from_directory
 import os
 from werkzeug.utils import secure_filename
+from PIL import Image
+import numpy as np
+from collections import Counter
+from time import time
+
 
 app = Flask(__name__)
 
@@ -14,29 +19,42 @@ os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
+def extract_top_colors(image_path, top_n=10):
+    img = Image.open(image_path)
+    img.thumbnail((200, 200))
+    img = img.convert('RGB')
+    data = np.array(img)
+    pixels = data.reshape((-1, 3))
+
+    counts = Counter(map(tuple, pixels))
+    total_pixels = len(pixels)
+    top_colors = counts.most_common(top_n)
+    
+    color_data = []
+    for color, count in top_colors:
+        percentage = f"{round((count / total_pixels) * 100, 3)}%"
+        hex_color = '#{:02x}{:02x}{:02x}'.format(*color)
+        color_data.append((hex_color, percentage))
+        
+    return color_data
+
+
 @app.route("/", methods=['GET', 'POST'])
 def home():
-    color_data = {
-        "#481830": 0.301746,
-        "#783030": 0.212460,
-        "#306078": 0.166667,
-        "#486090": 0.084683,
-        "#183060": 0.082381,
-        "#d86048": 0.053175,
-        "#a84830": 0.037222,
-        "#300018": 0.026349,
-        "#781818": 0.017857,
-        "#f0d860": 0.007857
-    }
+
+    default_image = "static/img/image.jpg"
+    color_data = extract_top_colors(default_image)
+
+
     filename = None
     if request.method == 'POST':
         file = request.files.get('image')
         if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
+            filename = "uploaded_image.jpg"
             file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
             file.save(file_path)
-    print(filename)
-    return render_template("index.html",colors=color_data, filename=filename)
+            color_data = extract_top_colors(file_path)
+    return render_template("index.html",colors=color_data, filename=filename, time=time)
 
 @app.route('/uploads/<filename>')
 def uploaded_file(filename):
